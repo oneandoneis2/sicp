1c883eed52e19db68c66b6f3132e03fa43869228
Dominic Humphries <dominich@broadbean.com>
2015-09-27 18:09:07 +0100

    Stop pretending we can't just sort the damn things

--- chap1.scm
+++ chap1.scm
@@ -1,7 +1,4 @@
 (define (sum-of-squares x y) (+ (square x) (square y)))
-(define (smallest_first? x y z) (and (<= x y)(<= x z)))
 (define (sos_gt x y z)
-  (cond
-    ((smallest_first? x y z) (sum-of-squares y z))
-    ((smallest_first? y x z) (sum-of-squares x z))
-    ((smallest_first? z x y) (sum-of-squares x y))))
+  (define vals (cdr (sort (list x y z) <)))
+  (sum-of-squares (car vals) (cadr vals)))
