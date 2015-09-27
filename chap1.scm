221d8fdcbe5112181a4fcdcf3efff8d158248527
Dominic Humphries <dominich@broadbean.com>
2015-09-27 18:35:34 +0100

    Explain normal vs. applicative outcomes

--- chap1.scm
+++ chap1.scm
@@ -2,3 +2,14 @@
 (define (sos_gt x y z)
   (define vals (cdr (sort (list x y z) <)))
   (sum-of-squares (car vals) (cadr vals)))
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+; ^ a-b == a+(-b) so use subtract function if b<0
+(define (p) (p))
+(define (test x y)
+  (if (= x 0)
+    0
+    y))
+; (test 0 (p))
+; applicative-order - attempts to evaluate (p) first -> endless loop
+; normal-order - (test 0 (p)) -> (if (= 0 0) 0 (p)) -> 0 - no need to evaluate (p)
