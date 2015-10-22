acb9d571f7982704c5ac6535ba7a694a10a2db0d
Dominic Humphries <dominich@broadbean.com>
2015-10-22 16:30:56 +0100

    Begin chapter 2!

---
+++ chap2.scm
@@ -0,0 +1,39 @@
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (numer y) (denom x))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+; Goes against the grain to not just do (define make-rat cons)
+; especially after reading the Little Schemer, which does,
+; but I accept their argument of it being better for debugging
+(define (make-rat x y)
+  (cons x y))
+
+(define (numer x)
+  (car x))
+
+(define (denom x)
+  (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
