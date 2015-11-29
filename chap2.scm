084467827fa8d3b30aee295ff34d4ba30d16726c
Dominic Humphries <dominich@broadbean.com>
2015-11-29 21:47:33 +0000

    Let's get symbolic!

--- chap2.scm
+++ chap2.scm
@@ -873,9 +873,9 @@
                      (make-segment mid-b mid-l)
                      (make-segment mid-l mid-t)))
 
-(define (frame-wave f)
+;(define (frame-wave f)
   ; TBD - I'll need the physical book and/or a grid system!
-  )
+;  )
 
 ; 2.50
 (define (flip-horiz painter)
@@ -936,3 +936,61 @@
   (let ((combine4 (square-of-four flip-vert rotate180
                                   identity flip-horiz)))
     (combine4 (corner-split painter n))))
+
+; 2.54
+(define (my_equal? a b)
+  (if (and (pair? a) (pair? b)) ; Can't eq? pairs...
+    (and (my_equal? (car a) (car b))
+         (my_equal? (cdr a) (cdr b)))
+    (eq? a b)))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        (else
+          (error "unknown expression type -- DERIV" exp))))
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (addend a) (cadr a))
+
+(define (augend a) (caddr a))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier p) (cadr p))
+
+(define (multiplicand p) (caddr p))
