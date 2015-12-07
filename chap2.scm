ca889a92619eae74e8e4fdc7f4250c6d1a10807c
Dominic Humphries <dominich@broadbean.com>
2015-12-07 17:02:56 +0000

    Ordered trees!

--- chap2.scm
+++ chap2.scm
@@ -1100,7 +1100,7 @@
         ((> x (car set)) (cons (car set) (adjoin-set x (cdr set))))
         (else (cons x set))))
 
-; 2.61
+; 2.62
 (define (union-set set1 set2)
   (cond ((null? set1) set2)
         ((null? set2) set1)
@@ -1109,3 +1109,98 @@
             (cond ((= x1 x2) (cons x1 (union-set (cdr set1) (cdr set2))))
                   ((< x1 x2) (cons x1 (union-set (cdr set1) set2)))
                   (else (cons x2 (union-set set1 (cdr set2)))))))))
+
+(define fig2-16-1 '(7 (3 (1 () ()) (5 () ())) (9 () (11 () ()))))
+(define fig2-16-2 '(3 (1 () ()) (7 (5 () ()) (9 () (11 () ())))))
+(define fig2-16-3 '(5 (3 (1 () ()) ()) (9 (7 () ()) (11 () ()))))
+
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+    '()
+    (append (tree->list-1 (left-branch tree))
+            (cons (entry tree)
+                  (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+      result-list
+      (copy-to-list (left-branch tree)
+                    (cons (entry tree)
+                          (copy-to-list (right-branch tree)
+                                        result-list)))))
+  (copy-to-list tree '()))
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+    (cons '() elts)
+    (let ((left-size (quotient (- n 1) 2)))
+      (let ((left-result (partial-tree elts left-size)))
+        (let ((left-tree (car left-result))
+              (non-left-elts (cdr left-result))
+              (right-size (- n (+ left-size 1))))
+          (let ((this-entry (car non-left-elts))
+                (right-result (partial-tree (cdr non-left-elts)
+                                            right-size)))
+            (let ((right-tree (car right-result))
+                  (remaining-elts (cdr right-result)))
+              (cons (make-tree this-entry left-tree right-tree)
+                    remaining-elts))))))))
+
+; 2.64
+; So, taking the magical handwavy approach: We have an ordered list,
+; we want an ordered tree. That list consists of:
+;  zero or more low values
+;  zero or one middle value
+;  zero or more high values
+; So our list of (lows, middle, highs) needs to be turned into ([lows], middle, [highs])
+; where []s indicate trees. Imagine we have a list->tree function already, called magic.
+; The effect we want is (list (magic lows) middle (magic highs))
+; But that's a little tricky, since we want to just use car/cdr and that doesn't scale so
+; well for the highs part. But hey, a tree is a single list! So if the highs are tree'd,
+; things are easy. Let's imagine magic takes a list and a number, and returns a list
+; where the first element is the first <number> of elements, correctly tree'd, then we'd have
+; (magic list) -> ([lows] middle highs) - then middle is just the cadr, and the list we
+; want to pass to magic for the right tree is just cddr.
+; So we're looking for a function that takes a list and a number, and trees that number of
+; entries in a list. Hello, partial-tree!
+; The quotient call is what splits the list to find our middle entry and tree the others;
+; when n hits 0 it returns an empty tree; and recursion basically does the rest!
+; ...this would be a lot nicer with labels rather than let-let-let-let-let!
+
+; (1 3 5 7 9 11) ->     5
+;                      / \
+;                     /   \
+;                    1     \
+;                     \     9
+;                      3   / \
+;                         7   11
