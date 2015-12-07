97c56d915817156a0b2485988c462ce95a68675e
Dominic Humphries <dominich@broadbean.com>
2015-12-07 17:19:36 +0000

    Trivial stuff, really

--- chap2.scm
+++ chap2.scm
@@ -1204,3 +1204,13 @@
 ;                     \     9
 ;                      3   / \
 ;                         7   11
+;
+; 2.65
+; It's just tree->list -> union/intersection -> list->tree
+(define (union-tree tree1 tree2)
+  (list->tree (union-set (tree->list tree1)
+                         (tree->list tree2))))
+
+(define (intersection-tree tree1 tree2)
+  (list->tree (intersection-set (tree->list tree1)
+                                (tree->list tree2))))
