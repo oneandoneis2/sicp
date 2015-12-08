13b28fff928135694fce49475ac7950a724214d6
Dominic Humphries <dominich@broadbean.com>
2015-12-08 16:33:39 +0000

    Let's encode!

--- chap2.scm
+++ chap2.scm
@@ -1224,3 +1224,96 @@
          (tree-lookup given-key (left-branch set-of-records)))
         (else
          (tree-lookup given-key (right-branch set-of-records)))))
+
+; Huffman
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch tree) (car tree))
+
+(define (right-branch tree) (cadr tree))
+(define (symbols tree)
+  (if (leaf? tree)
+    (list (symbol-leaf tree))
+    (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+    (weight-leaf tree)
+    (cadddr tree)))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+      '()
+      (let ((next-branch
+              (choose-branch (car bits) current-branch)))
+        (if (leaf? next-branch)
+          (cons (symbol-leaf next-branch)
+                (decode-1 (cdr bits) tree))
+          (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit -- CHOOSE-BRANCH" bit))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+    '()
+    (let ((pair (car pairs)))
+      (adjoin-set (make-leaf (car pair)    ; symbol
+                             (cadr pair))  ; frequency
+                  (make-leaf-set (cdr pairs))))))
+
+; 2.67
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                    (make-leaf 'B 2)
+                    (make-code-tree (make-leaf 'D 1)
+                                    (make-leaf 'C 1)))))
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+(decode sample-message sample-tree) ; (a d a b b c a)
+
+; 2.68
+(define (encode message tree)
+  (if (null? message)
+    '()
+    (append (encode-symbol (car message) tree)
+            (encode (cdr message) tree))))
+
+(define (in-list? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (in-list? x (cdr set)))))
+
+(define (encode-symbol symbol tree)
+  (define (correct-branch? symbol branch)
+    (cond ((leaf? branch) (eq? symbol (symbol-leaf branch)))
+          ((in-list? symbol (symbols branch)))))
+  (cond ((and (leaf? tree) (correct-branch? symbol tree)) '())
+        ((correct-branch? symbol (left-branch tree))
+         (cons 0 (encode-symbol symbol (left-branch tree))))
+        ((correct-branch? symbol (right-branch tree))
+         (cons 1 (encode-symbol symbol (right-branch tree))))
+        (else (error "bad symbol -- ENCODE-SYMBOL" symbol))))
+
