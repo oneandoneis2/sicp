cdb0bf380382799d8fda0758e76b9f72ffaa2f01
Dominic Humphries <dominich@broadbean.com>
2016-01-07 18:00:35 +0000

    2.81

--- chap2.scm
+++ chap2.scm
@@ -1707,3 +1707,32 @@
 (put 'imag-part '(complex) imag-part)
 (put 'magnitude '(complex) magnitude)
 (put 'angle '(complex) angle)
+
+; 2.81 a
+; Infinite loop, it will keep trying to apply-generic on what it THINKS are 'raised' params
+; b
+; So long as we have a valid process in the table for the two types of operand, there is no
+; problem - coercion is only tried when no process is found.
+; c
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+        (apply proc (map contents args))
+        (if (= (length args) 2)
+          (let ((type1 (car type-tags))
+                (type2 (cadr type-tags))
+                (a1 (car args))
+                (a2 (cadr args)))
+            (if (not (equal? type1 type2))
+              (let ((t1->t2 (get-coercion type1 type2))
+                    (t2->t1 (get-coercion type2 type1)))
+                (cond (t1->t2
+                        (apply-generic op (t1->t2 a1) a2))
+                      (t2->t1
+                        (apply-generic op a1 (t2->t1 a2)))
+                      (else
+                        (error "No method for these types"
+                               (list op type-tags)))))))
+          (error "No method for these types"
+                 (list op type-tags)))))))
