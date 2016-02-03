ed7cb97d23aad43962f56dfece3fe2d1c33b78fc
Dominic Humphries <dominich@broadbean.com>
2016-02-03 16:34:41 +0000

    3.24

--- chap3.scm
+++ chap3.scm
@@ -392,3 +392,39 @@
               (set-front-dq-ptr! dq '())
               (set-de-next! prev '()))
             (set-rear-dq-ptr! dq prev)))))
+
+; 3.24
+(define (make-table . equality)
+  (let ((local-table (list '*table*)))
+    (let ((same-key? (if (null? equality) equal? (car equality))))
+      (define (my-assoc key records)
+        (cond ((null? records) false)
+              ((same-key? key (caar records)) (car records))
+              (else (my-assoc key (cdr records)))))
+      (define (lookup key-1 key-2)
+        (let ((subtable (my-assoc key-1 (cdr local-table))))
+          (if subtable
+            (let ((record (my-assoc key-2 (cdr subtable))))
+              (if record
+                (cdr record)
+                false))
+            false)))
+      (define (insert! key-1 key-2 value)
+        (let ((subtable (my-assoc key-1 (cdr local-table))))
+          (if subtable
+            (let ((record (my-assoc key-2 (cdr subtable))))
+              (if record
+                (set-cdr! record value)
+                (set-cdr! subtable
+                          (cons (cons key-2 value)
+                                (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table)))))
+        'ok)
+      (define (dispatch m)
+        (cond ((eq? m 'lookup-proc) lookup)
+              ((eq? m 'insert-proc!) insert!)
+              (else (error "Unknown operation -- TABLE" m))))
+      dispatch)))
