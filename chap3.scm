e11ea747f368a87303d1260875fd87db9775d405
Dominic Humphries <dominich@broadbean.com>
2016-02-12 12:02:20 +0000

    Adders

--- chap3.scm
+++ chap3.scm
@@ -511,3 +511,98 @@
             ((eq? m 'display) (show-tree))
             (else (error "Unknown operation -- TABLE" m))))
     dispatch))
+
+; 3.27
+; memo-fib only calculates each value once, therefore linear growth in steps
+; Wouldn't work, because fib calls itself for recursion, which would bypass the memoization
+; of the values it returns.
+
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+            (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (logical-and s1 s2)
+  (cond ((and (= s1 1) (= s2 1)) 1)
+        ((and (= s1 1) (= s2 0)) 0)
+        ((and (= s1 0) (= s2 1)) 0)
+        ((and (= s1 0) (= s2 0)) 0)
+        (else (error "Invalid signal" s))))
+
+(define (logical-or s1 s2)
+  (cond ((and (= s1 1) (= s2 1)) 1)
+        ((and (= s1 1) (= s2 0)) 1)
+        ((and (= s1 0) (= s2 1)) 1)
+        ((and (= s1 0) (= s2 0)) 0)
+        (else (error "Invalid signal" s))))
+
+; 3.28
+(define (or-gate o1 o2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+            (logical-or (get-signal o1) (get-signal o2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! o1 or-action-procedure)
+  (add-action! o2 or-action-procedure)
+  'ok)
+
+; 3.29
+(define (or-gate o1 o2 output)
+  (let ((no1 (make-waire))
+        (no2 (make-wire))
+        (aout (make-wire)))
+    (inverter o1 no1)
+    (inverter o2 no2)
+    (and-gate no1 no2 aout)
+    (inverter aout output)
+    'ok))
+
+; 3.30
+(define (ripple-carry-adder list_a list_b list_s c_out)
+  (if (or (null? list_a) (null? list_b) (null? list_s))
+    (error "Prematurely exhausted list" (list list_a list_b list_s)))
+  (let ((c_in (make-wire)))
+    (if (and (null? (cdr list_a)) (null? (cdr list_b)) (null? (cdr list_s)))
+      (set-signal! c_in 0)
+      (iter (cdr list_a) (cdr list_b) (cdr list_s) c_in))
+    (full-adder (car list_a) (car list_b) c_in (car list_s) c_out))
+  'ok)
