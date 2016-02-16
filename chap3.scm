9363e00fd01591b9e9d71cbbca00e7cb447f7a7c
Dominic Humphries <dominich@broadbean.com>
2016-02-16 11:36:54 +0000

    Correct squarer

--- chap3.scm
+++ chap3.scm
@@ -922,3 +922,29 @@
 ; multiplier can only work out one operand when it knows the other, as well as the result
 ; It's not smart enough to work back from "result is 9, so operands are both 3"
 
+; 3.35
+(define (squarer a b)
+  (define (process-new-value)
+    (if (has-value? b)
+      (if (< (get-value b) 0)
+        (error "square less than 0 -- SQUARER" (get-value b))
+        (set-value! a
+                    (sqrt (get-value b))
+                    me))
+      (if (has-value? a)
+        (set-value! b
+                    (square (get-value a))
+                    me))))
+  (define (process-forget-value)
+    (forget-value! a me)
+    (forget-value! b me))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value)
+           (process-new-value))
+          ((eq? request 'I-lost-my-value)
+           (process-forget-value))
+          (else
+            (error "Unknown request -- SQUARER" request))))
+  (connect a me)
+  (connect b me)
+  me)
