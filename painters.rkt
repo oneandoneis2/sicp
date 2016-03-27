5a0689c9723b4e0b098e921cb6a6a6933ac814ca
Dominic Humphries <dominich@broadbean.com>
2016-03-27 15:59:38 +0100

    Remainind painter stuff

--- painters.rkt
+++ painters.rkt
@@ -1,16 +1,16 @@
 #lang sicp
 (#%require sicp-pict)
 
+;2.52
 (define (corner-split painter n)
   (if (= n 0)
-      painter
-      (let ((up (up-split painter (- n 1)))
-            (right (right-split painter (- n 1))))
-        (let ((top-left (beside up up))
-              (bottom-right (below right right))
-              (corner (corner-split painter (- n 1))))
-          (beside (below painter top-left)
-                  (below bottom-right corner))))))
+    painter
+    (beside (below painter (up-split painter (- n 1)))
+            (below (right-split painter (- n 1)) (corner-split painter (- n 1))))))
+
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four flip-horiz identity rotate180 flip-vert)))
+    (combine4 (corner-split painter n))))
 
 (define (square-of-four tl tr bl br)
   (lambda (painter)
@@ -32,3 +32,33 @@
         (op1 painter (op2 smaller smaller))))))
 (define right-split (split beside below))
 (define up-split (split below beside))
+
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (frame-origin frame)
+     (add-vect (scale-vect (xcor-vect v)
+                           (frame-edge1 frame))
+               (scale-vect (ycor-vect v)
+                           (frame-edge2 frame))))))
+
+; 2.46
+(define (make-vect a b)
+  (cons a b))
+(define (xcor-vect v)
+  (car v))
+(define (ycor-vect v)
+  (cdr v))  ; cons not list - cdr not cadr!
+(define (add-vect a b)
+  (make-vect (+ (xcor-vect a)
+                (xcor-vect b))
+             (+ (ycor-vect a)
+                (ycor-vect b))))
+(define (sub-vect a b)
+  (make-vect (- (xcor-vect a)
+                (xcor-vect b))
+             (- (ycor-vect a)
+                (ycor-vect b))))
+(define (scale-vect n v)
+  (make-vect (* n (xcor-vect v))
+             (* n (ycor-vect v))))
