ac09ac3f9f1d47334acf3f5d595dc4d41d2db52c
Dominic Humphries <dominich@broadbean.com>
2016-07-26 17:23:18 +0100

    Ex 5.8

--- chap5.scm
+++ chap5.scm
@@ -12,3 +12,24 @@
       (assign count (reg tmp))
       (goto (label test-b))
   fact-done)
+
+; 5.8
+
+(define (extract-labels text receive)
+  (if (null? text)
+      (receive '() '())
+      (extract-labels (cdr text)
+       (lambda (insts labels)
+         (let ((next-inst (car text)))
+           (if (symbol? next-inst)
+             (if (label-present? next-inst labels)
+               (error "Label defined twice -- " next-inst)
+               (receive insts
+                        (cons (make-label-entry next-inst
+                                                insts)
+                              labels)))
+               (receive (cons (make-instruction next-inst)
+                              insts)
+                        labels)))))))
+
+(define label-present? assoc)
