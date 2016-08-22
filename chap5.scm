0dc7bd515065f436de62e2be59f51ea2eeee7e00
Dominic Humphries <dominich@broadbean.com>
2016-08-22 11:51:07 +0100

    Ex 5.21a

--- chap5.scm
+++ chap5.scm
@@ -29,3 +29,49 @@
    (restore continue)
    (assign val                        ;  Fib(n - 2) +  Fib(n - 1)
            (op +) (reg val) (reg n))
+
+; 5.21 a
+(define (count-leaves tree)
+  (cond ((null? tree) 0)
+        ((not (pair? tree)) 1)
+        (else (+ (count-leaves (car tree))
+                 (count-leaves (cdr tree))))))
+
+(define count-leaves-machine
+  (make-machine ; assuming the dynamic register allocation from 5.13
+    (list (list 'null? null?) (list 'pair? pair?) (list '+ +) (list 'car car) (list 'cdr cdr))
+    '(
+      (assign val (const 0))
+      (assign continue (label count-done))
+      count
+          (test (op null?) (reg tree))  ; Handle null
+          (branch (label null-tree))
+          (test (op pair?) (reg tree))
+          (branch (label count-pair))   ; Handle pair
+          (assign val (const 1))        ; Handle leaf
+          (goto (reg continue))
+      count-pair
+      ; If it's a pair, total the car then continue to the cdr
+          (save continue)
+          (save tree)
+          (assign continue (label count-cdr))
+          (assign tree (op car) (reg tree))
+          (goto (label count))
+      count-cdr
+      ; Handle cdr by saving current tree then summing the subtree
+          (restore tree)
+          (save val)
+          (assign tree (op cdr) (reg tree))
+          (assign continue (label after-pair))
+          (goto (label count))
+      after-pair
+      ; Get the cdr value into var so we can restore val, then sum them
+          (assign var (reg val))
+          (restore val)
+          (assign val (op +) (reg val) (reg var))
+          (restore continue)
+          (goto (reg continue))
+      null-tree
+          (assign val (const 0))
+          (goto (reg continue))
+      count-done)))
